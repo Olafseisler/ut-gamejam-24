d83150cc53af64f5dba90df5966925c8e7dd65cb
Olaf Seisler <olaf.seisler@gmail.com>
2024-04-13 19:13:06 +0300

    Added UI elements for summoning minions.

---
+++ Assets/Scenes/UI/UIController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Transform summonPosition;
+    [SerializeField] private GameObject summonSelectButton;
+    public GameObject summonButtonParent;
+    [SerializeField] List<GameObject> summons;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i < summons.Count; i++)
+        {
+            var go = Instantiate(summonSelectButton, summonButtonParent.transform);
+            go.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = summons[i].name;
+            var image = go.GetComponentsInChildren<Image>()[1];
+            image.sprite = summons[i].GetComponent<SpriteRenderer>().sprite;
+            var index = i;
+            go.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                var go1 = Instantiate(summons[index], summonPosition.position, Quaternion.identity);
+                var minionScript = go1.GetComponent<MeeleSummon>();
+                minionScript.SetupFriendly();
+            });
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
