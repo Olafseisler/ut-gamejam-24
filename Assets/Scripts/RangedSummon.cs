e1e2d0423c161ce25cc9f17a551dba6abbb38223
VirtuousDemon <rasmus.lille0@gmail.com>
2024-04-13 14:26:43 +0300

    Summon Scripts

--- Assets/Scripts/RangedSummon.cs
+++ Assets/Scripts/RangedSummon.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
-public class MeeleSummon : MonoBehaviour
+public class RangedSummon : MonoBehaviour
 {
     [SerializeField] private float health;
     [SerializeField] private float dmg;
     [SerializeField] private float speed;
-    [SerializeField] private int direction; 
+    [SerializeField] private int direction;
+    [SerializeField] private float range;
+    [SerializeField] private bool inRange = false;
     [SerializeField] private bool inCombat = false;
     [SerializeField] private bool waiting = false;
     [SerializeField] private string enemyTag;
@@ -17,17 +22,19 @@ public class MeeleSummon : MonoBehaviour
     void Start()
     {
         speed *= direction;
+        //ContactFilter2D filter = new ContactFilter2D();
+        //filter.layerMask =  1 << LayerMask.NameToLayer("Enemy");
     }
 
     private void OnCollisionEnter2D(Collision2D other)
-    {
+    { 
         if (other.gameObject.CompareTag(enemyTag)) inCombat = true;
         else if (other.gameObject.CompareTag(gameObject.tag)) waiting = true;
     }
 
     private void OnCollisionExit2D()
     {
-        inCombat = waiting = false;
+        waiting = false;
     }
 
     // Update is called once per frame
@@ -37,11 +44,18 @@ public class MeeleSummon : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right , range, 1 << LayerMask.NameToLayer(enemyTag));
+        if (hit.collider) inRange = true;
+        else inRange = false;
+        
         if (inCombat)
         {
-            combat(Time.deltaTime);
+            closeCombat(Time.deltaTime);
         }
-
+        else if (inRange)
+        {
+            rangedCombat(Time.deltaTime);
+        }
         else if (waiting)
         {
             idle(Time.deltaTime);
@@ -57,7 +71,12 @@ public class MeeleSummon : MonoBehaviour
         transform.position += Vector3.right * (speed * deltaTime);
     }
     
-    private void combat(float deltaTime)
+    private void closeCombat(float deltaTime)
+    {
+        
+    }
+    
+    private void rangedCombat(float deltaTime)
     {
         
     }
