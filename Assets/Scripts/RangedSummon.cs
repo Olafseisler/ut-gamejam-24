f93780880063cb19c9445245ed15ec1bd76d9a33
VirtuousDemon <rasmus.lille0@gmail.com>
2024-04-13 17:47:56 +0300

    RangedSummon overhaul

--- Assets/Scripts/RangedSummon.cs
+++ Assets/Scripts/RangedSummon.cs
@@ -18,55 +18,65 @@ public class RangedSummon : MonoBehaviour
     [SerializeField] private bool inCombat = false;
     [SerializeField] private bool waiting = false;
     [SerializeField] private string enemyTag;
+    private string _friendlyTag;
+    private float _vahe = 0.1f;
+    private Vector3 _height_2;
+    private Vector3 _width_2;
     // Start is called before the first frame update
     void Start()
     {
         speed *= direction;
+        _vahe *= direction;
+        _friendlyTag = gameObject.tag;
+        _height_2 = new Vector3(0, gameObject.GetComponent<Collider2D>().bounds.size.y / 2);
+        _width_2 = new Vector3(gameObject.GetComponent<Collider2D>().bounds.size.x / 2 + 0.01f, 0) * direction;
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    { 
-        if (other.gameObject.CompareTag(enemyTag)) inCombat = true;
-        else if (other.gameObject.CompareTag(gameObject.tag)) waiting = true;
-    }
-    
-    // private void OnCollisionExit2D()
-    // {
-    //     waiting = false;
+    // private void OnCollisionEnter2D(Collision2D other)
+    // { 
+    //     if (other.gameObject.CompareTag(enemyTag)) inCombat = true;
     // }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        // //Waiting
+        // if (Physics2D.Raycast(transform.position + Vector3.up +
+        //                       new Vector3(gameObject.GetComponent<Collider2D>().bounds.size.x / 2 + 0.01f, 0), transform.position * direction, _vahe, 1 << LayerMask.NameToLayer(_friendlyTag))) waiting = true;
+        // else waiting = false;
+        
+        //InCOmbat
+        if (Physics2D.Raycast(transform.position + _height_2 + _width_2, _width_2 * direction, _vahe, 1 << LayerMask.NameToLayer(enemyTag))) inCombat = true;
+        else
+        {
+            inCombat = false;
+            //Waiting
+            if (Physics2D.Raycast(transform.position + _height_2 +
+                                  _width_2, _width_2 * direction, _vahe, 1 << LayerMask.NameToLayer(_friendlyTag))) waiting = true;
+            else waiting = false;
+        }
+    }
+
     void Update()
     {
         if (health <= 0)
         {
             Destroy(gameObject);
         }
-
-        if (!Physics2D.Raycast(
-                transform.position + Vector3.up +
-                new Vector3(gameObject.GetComponent<Collider2D>().bounds.size.x / 2 + 0.01f, 0),
-                transform.right * direction,
-                0.01f).collider) waiting = false;
-        if (Physics2D.Raycast(transform.position + Vector3.up, transform.right * direction, range, 1 << LayerMask.NameToLayer(enemyTag)).collider) inRange = true;
-        else inRange = false;
+        //InRange
+        // if (Physics2D.Raycast(transform.position + _height_2 + _width_2, _width_2, range, 1 << LayerMask.NameToLayer(enemyTag)).collider) inRange = true;
+        // else inRange = false;
         
-        if (inCombat)
-        {
-            closeCombat(Time.deltaTime);
-        }
-        else if (inRange)
-        {
-            rangedCombat(Time.deltaTime);
-        }
-        else if (waiting)
-        {
-            idle(Time.deltaTime);
-        }
+        if (inCombat) closeCombat(Time.deltaTime);
         else
         {
-            move(Time.deltaTime);
+            //InRange
+            if (Physics2D.Raycast(transform.position + _height_2 + _width_2, _width_2, range, 1 << LayerMask.NameToLayer(enemyTag)).collider) inRange = true;
+            else inRange = false;
+            if(inRange){rangedCombat(Time.deltaTime);}
+            else if (waiting)idle(Time.deltaTime);
+            else move(Time.deltaTime);
         }
+
     }
 
     private void move(float deltaTime)
@@ -81,7 +91,7 @@ public class RangedSummon : MonoBehaviour
     
     private void rangedCombat(float deltaTime)
     {
-        
+        if (!waiting) move(deltaTime);
     }
     
     private void idle(float deltaTime)
@@ -91,6 +101,8 @@ public class RangedSummon : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position, transform.position + new Vector3(range, 0) * direction);
+        // Gizmos.DrawLine(transform.position + _height_2, transform.position + _height_2 + new Vector3(range, 0) * direction);
+        Gizmos.DrawLine(transform.position + _height_2 +
+                        _width_2, transform.position + _height_2 + new Vector3(_vahe, 0));
     }
 }
